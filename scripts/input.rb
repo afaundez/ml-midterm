4d0a89c274d5ceaf905ef23086b604e3cbfdaaa7
Alvaro Faundez <alvaro@faundez.net>
2020-09-25 00:49:00 -0400

    Refactor input processing

--- scripts/input.rb
+++ scripts/input.rb
@@ -1,99 +1,149 @@
 require 'set'
 
-header = STDIN.readline.chomp
-header_list = header.split(',')
-class_name = header_list[0]
-measurement_names = Set.new header_list[1..-1]
+class DataSet
+  attr_accessor :dimensions, :classes, :measurements, :measurement_count_by_category,
+    :measurement_count, :measurement_count_by_measurement_dimension_and_category,
+    :category_count_by_measurement
+  def initialize(header)
+    header_list = header.split(',')
+    @dimensions = Set.new header_list[1..-1]
+    @categories = Set.new
+    @measurements_by_dimension = @dimensions.collect { |name| [name, Set.new] }.to_h
+    @measurement_count_by_category = {}
+    @measurement_count_by_dimension = @dimensions.collect { |name| [name, {}] }.to_h
+    @measurement_count_by_measurement_dimension_and_category = {}
+    @category_count_by_measurement = {}
+  end
 
-classes = Set.new
-measurements = measurement_names.collect { |name| [name, Set.new] }.to_h
-dataset_classes_count = {}
-dataset_measurements_count = measurement_names.collect { |name| [name, {}] }.to_h
-dataset_classes_and_measurements_count = {}
-dataset_measurements_classes_count = {}
+  def d_and_not_c_size(measurement, category)
+    @category_count_by_measurement.fetch(measurement, {})
+                                  .select{ |c| !c.eql?(category)}
+                                  .values
+                                  .inject(0.0, :+)
+  end
 
-STDIN.each_line do |line|
-  values = line.chomp.split(',')
-  class_value, *measurement_values = values
-  classes.add class_value
-  dataset_classes_count[class_value] ||= 0.0
-  dataset_classes_count[class_value] += 1.0
-  current_measurements = measurement_names.zip measurement_values
-  next if measurement_values.include?('.') || measurement_values.include?('NA')
-  dataset_measurements_classes_count[measurement_values] ||= {}
-  dataset_measurements_classes_count[measurement_values][class_value] ||= 0.0
-  dataset_measurements_classes_count[measurement_values][class_value] += 1.0
-  current_measurements.each do |measurement_name, measurement_value|
-    measurements[measurement_name].add measurement_value
-    dataset_measurements_count[measurement_name][measurement_value] ||= 0.0
-    dataset_measurements_count[measurement_name][measurement_value] += 1.0
+  def d_and_c_size(measurement, category)
+    @category_count_by_measurement.fetch(measurement, {})
+                                  .fetch(category, 0.0)
+  end
 
-    dataset_classes_and_measurements_count[class_value] ||= {}
-    dataset_classes_and_measurements_count[class_value][measurement_name] ||= {}
-    dataset_classes_and_measurements_count[class_value][measurement_name][measurement_value] ||= 0.0
-    dataset_classes_and_measurements_count[class_value][measurement_name][measurement_value] += 1.0
+  def c_size(category)
+    @measurement_count_by_category.fetch(category)
+  end
+
+  def size
+    measurement_count_by_category.values.inject(0.0, :+)
+  end
+
+  def K
+    @categories.to_a
+  end
+
+  def M_names
+    @M_names ||= @measurements_by_dimension.collect { |name, values|  values.to_a }
+  end
+
+  def M
+    @M ||= @measurements_by_dimension.each_with_index.collect do |(name, values), index|
+      dimension = index + 1
+      values.size
+    end
+  end
+
+  def N
+    @N ||= self.M.size
+  end
+
+  def L
+    @L ||= self.M.collect { |m| (0..(m - 1)).to_a }
+  end
+
+  def linear_address(d)
+    accumulate_gaps = @M.inject([1]) { |acc, m_i| acc << acc.last * m_i }.take @M.size
+    return accumulate_gaps.zip(d).map { |k, v| k*v }.inject(:+)
+  end
+
+  def add_category(category)
+    @categories.add category
+    @measurement_count_by_category[category] ||= 0.0
+    @measurement_count_by_category[category] += 1.0
+    @measurement_count_by_measurement_dimension_and_category[category] ||= {}
+  end
+
+  def add_class_and_measurement(category, measurements)
+    count_measurement_by_class category, measurements
+    measurments_by_dimension = @dimensions.zip measurements
+    measurments_by_dimension.each do |dimension, measurement|
+      @measurements_by_dimension[dimension].add measurement
+      @measurement_count_by_measurement_dimension_and_category[category][dimension] ||= {}
+      count_measurement_by_dimension dimension, measurement
+      count_measurement_by_dimension_and_class category, dimension, measurement
+    end
+  end
+
+  private
+
+  def count_measurement_by_dimension_and_class(category, dimension, measurement)
+    @measurement_count_by_measurement_dimension_and_category[category][dimension][measurement] ||= 0.0
+    @measurement_count_by_measurement_dimension_and_category[category][dimension][measurement] += 1.0
+  end
+
+  def count_measurement_by_dimension(dimension, measurement)
+    @measurement_count_by_dimension[dimension][measurement] ||= 0.0
+    @measurement_count_by_dimension[dimension][measurement] += 1.0
+  end
+
+  def count_measurement_by_class(category, measurement)
+    @category_count_by_measurement[measurement] ||= {}
+    @category_count_by_measurement[measurement][category] ||= 0.0
+    @category_count_by_measurement[measurement][category] += 1.0
   end
 end
 
-input_K = classes.to_a
-puts "K = {#{input_K.join(',')}}"
-puts "|K| = #{input_K.size}"
+header = STDIN.readline.chomp
+dataset = DataSet.new header
 
-input_N = measurement_names.to_a.size
-puts "N = #{input_N}"
-
-measurements.each_with_index do |(name, values), index|
-  dimension = index + 1
-  puts "D#{dimension} = {#{values.to_a.join(', ')}}. M#{dimension} = |D#{dimension}| = #{values.size}"
+STDIN.each_line do |line|
+  category, *measurements = line.chomp.split(',')
+  dataset.add_category category
+  next if measurements.include?('.') || measurements.include?('NA')
+  dataset.add_class_and_measurement category, measurements
 end
 
-dataset_size = dataset_classes_count.values.inject(0.0, :+)
-prior_pr = dataset_classes_count.collect do |name, amount|
-  [name, amount/dataset_size]
-end.to_h
+puts "K = categories = { #{dataset.K.join(', ')} }"
+puts "|K| = #{dataset.K.size}"
+puts "L = { #{dataset.L.collect{|x| "{ #{x.join(', ')} }" }.join(',  ')} }"
+puts "N = |L| = #{dataset.N}"
+puts "M = { |L_0|, ..., |L_(N-1)| }= { #{dataset.M.join(', ')} }"
 
-prior_pr.each do |name, value|
-  puts "P(C=#{name}) = #{value}"
+def e_for(true_category, assigned_category)
+  true_category == assigned_category ? 1 : -1
 end
 
-def e_for(true_class_name, assigned_class_name)
-  true_class_name == assigned_class_name ? 1 : -1
-end
-
-def pr_c_given_D_for(class_name, measurements, prior_pr, dataset_classes_and_measurements_count, dataset_measurements_count, dataset_measurements_classes_count, dataset_classes_count)
-  dataset_c_size = dataset_classes_count.fetch(class_name)
-  dataset_size = dataset_classes_count.values.inject(0.0, :+)
-
-  # P(c)
-  pr_c = dataset_c_size / dataset_size
-
-  # P(~c)
+def pr_c_given_D_for(category, measurement, dataset)
+  pr_c = dataset.c_size(category) / dataset.size
+  pr_d_and_c = dataset.d_and_c_size(measurement, category) / dataset.size
+  pr_d_given_c = pr_d_and_c / pr_c
+  pr_d_and_not_c = dataset.d_and_not_c_size(measurement, category) / dataset.size
   pr_not_c = 1.0 - pr_c
-
-  # P(D | c)
-  dataset_D_and_c_size = dataset_measurements_classes_count.fetch(measurements.values, {})
-                                                           .fetch(class_name, 0.0)
-  pr_D_and_c = (dataset_D_and_c_size / dataset_size)
-  pr_D_given_c = pr_D_and_c / pr_c
-
-
-  # P(D, ~c)
-  dataset_D_and_not_c_size = dataset_measurements_classes_count.fetch(measurements.values, {})
-                                                                 .select{ |c| !c.eql?(class_name)}
-                                                                 .values
-                                                                 .inject(0.0, :+)
-  pr_D_and_not_c = dataset_D_and_not_c_size / dataset_size
-  pr_D_given_not_c = pr_D_and_not_c / pr_not_c
-
-  # P(D)
-  pr_D = pr_D_given_c * pr_c + pr_D_given_not_c * pr_not_c
-
-  # P(c | D) = P(D | c) * P(c) / P(D)
-  pr_D > 0 ? ((pr_D_given_c * pr_c) / pr_D) : 0.0
+  pr_d_given_not_c = pr_d_and_not_c / pr_not_c
+  pr_d = (pr_d_given_c * pr_c) + (pr_d_given_not_c * pr_not_c)
+  return 0.0 if pr_d <= 0
+  (pr_d_given_c * pr_c) / pr_d
 end
 
-classes.to_a.product(*measurements.values.collect(&:to_a)).each do |class_name, *measurement_values|
-  current_measurements = measurement_names.zip(measurement_values).to_h
-  value = pr_c_given_D_for class_name, current_measurements, prior_pr, dataset_classes_and_measurements_count, dataset_measurements_count, dataset_measurements_classes_count, dataset_classes_count
-  puts "Pr(c = #{class_name} | D = { #{current_measurements.values.join(',')} }) = #{value}" if value > 0
+def d_as_measurements_by_dimension(d, d_names, d_values_names)
+  d_value = d.each_with_index.collect { |dn, n| d_values_names[n][dn] }
+  d_names.zip(d_value).to_h
 end
+
+p_c_given_D = dataset.K.each_with_index.collect { |c, i| [i, []] }.to_h
+p_c_given_D.keys.to_a.product(*(dataset.L)).each do |c, *d|
+  category = dataset.K[c]
+  measurements = d_as_measurements_by_dimension d, dataset.dimensions.to_a, dataset.M_names
+  p_c_given_D[c][dataset.linear_address(d)] = pr_c_given_D_for category, measurements.values, dataset
+  value = p_c_given_D[c][dataset.linear_address(d)]
+  puts "Pr(c = #{category} | D = { #{measurements.values.join(',')} }) = #{value}" if value > 0
+end
+
+p p_c_given_D
